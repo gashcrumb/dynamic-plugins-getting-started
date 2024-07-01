5d31fc3cb9b4a02e8d6dc51b5589ae95097657db
Stan Lewis <gashcrumb@gmail.com>
2024-07-01 11:42:39 -0400

    feat: add initial simple-chat-backend from yarn new and ensure it's running in the app

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -38,4 +38,6 @@ backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+backend.add(import('@internal/backstage-plugin-simple-chat-backend'));
+
 backend.start();
