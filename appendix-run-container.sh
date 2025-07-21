6e161ea41e216da552cb3ead515480754c55ecbf
Stan Lewis <gashcrumb@gmail.com>
2025-07-21 11:35:38 -0400

    More cleanup

--- appendix-run-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-podman run -v ./deploy:/opt/app-root/src/dynamic-plugins-root:Z -v ./app-config.local.yaml:/opt/app-root/src/app-config.local.yaml:Z -p 7007:7007 --entrypoint='["node", "packages/backend", "--config", "app-config.yaml", "--config", "app-config.example.yaml", "--config", "app-config.local.yaml"]' quay.io/janus-idp/backstage-showcase:next
